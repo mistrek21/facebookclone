f87a234365bf32a5edb436797fff13b740646c03
mata <mata@matas-MacBook-Air.local>
2021-08-16 18:47:18 +0200

    header mid section

--- components/Header.js
+++ components/Header.js
@@ -11,12 +11,14 @@ import {
   FlagIcon,
   PlayIcon,
   SearchIcon,
-  ShoopingCartIcon,
+  ShoppingCartIcon,
 } from "@heroicons/react/outline";
+
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <div className="">
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -30,11 +32,20 @@ function Header() {
           <input
             type="text"
             placeholder="Search Facebook"
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
           />
         </div>
       </div>
       {/* Center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
       {/* Right */}
     </div>
   );
