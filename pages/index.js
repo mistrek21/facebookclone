5fb48160eb233640da190e9ef8c5514b7d85beac
mata <mata@matas-MacBook-Air.local>
2021-08-17 15:45:54 +0200

    add stories

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Header from "../components/Header";
-import Login from '../components/Login';
+import Login from "../components/Login";
 import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
 
 export default function Home({ session }) {
   if (!session) return <Login />;
 
   return (
-    <div>
+    <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
         <title>Facebook</title>
       </Head>
@@ -17,7 +18,7 @@ export default function Home({ session }) {
 
       <main className="flex">
         <Sidebar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </main>
     </div>
