50473db46738459663439c7d6cbe60b5e9583051
mata <mata@matas-MacBook-Air.local>
2021-08-17 02:01:23 +0200

    add next auth

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
+import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Header from "../components/Header";
+import Login from '../components/Login';
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
+
   return (
     <div>
       <Head>
         <title>Facebook</title>
       </Head>
 
-      {/* Header */}
       <Header />
 
       <main>
@@ -18,4 +21,15 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // Get the user
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
